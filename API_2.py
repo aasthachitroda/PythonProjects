0a2f48656faa6bb3a25830c4b0e7ecf7346a9990
Aastha Chitroda <144585273+aasthachitroda@users.noreply.github.com>
2023-11-14 09:21:41 +0530

    Add files via upload

---
+++ API_2.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def Home():
+    return "Home Page"
+
+@app.route('/armstrong/<int:n>')
+def armstrong(n):
+    sum = 0
+    order = len(str(n))
+    copy_n = n
+    while(n>0):
+        digit = n%10
+        sum += digit **order
+        n = n//10
+
+    if (sum == copy_n):
+        result = {
+            "Number": copy_n,
+            "Armstrong": True
+        }
+    else:
+        result = {
+            "Number": copy_n,
+            "Armstrong": False
+        }
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
